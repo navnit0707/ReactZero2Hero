c43a87a4606c673f20a3facc1578f6cdc48b7796
navnit0707 <knavnit81@gmail.com>
2023-01-11 00:52:01 +0530

    created a data.js file for notes so that i can refactor my code later to map the data to inject into html

---
+++ notes/data.js
@@ -0,0 +1,26 @@
+data = {
+    question1: {
+        question: "1. Difference between a Library and Framework?",
+        answer: `<p>Ans :<p> A library and a framework are both collections of pre-written code that can be used to perform common
+        tasks, but they are different in the way they are used and how they structure your code.
+</p>
+<p> A library is a collection of functions that you can call from your own code. A library does not dictate the
+structure of your code or how you should organize your application. It is a way to reuse code that you can
+integrate into your own projects, to make your development process faster. For example jQuery, Lodash, and
+moment.js are popular JavaScript libraries.
+</p>
+      <p>A framework, on the other hand, is a set of conventions and pre-written code that dictate the structure of
+        your application and the flow of control. A framework provides a set of rules that you must follow in order
+        to use it, and it takes control of the flow of your application. The framework calls your code, instead of
+        you calling the framework. For example React, Angular, Vue.js are popular JavaScript frameworks.
+</p> 
+        <p>A good way to think about it is that a library is a tool that you can use to do something, and a framework
+            is a set of tools that you use to structure your project.
+</p>
+        <p> It is worth noting that the distinction between libraries and frameworks can be blurry at times. Some
+            libraries have an optional set of conventions and best practices, which when followed provide a similar
+            structure to a framework.</p></p>
+     `
+
+    }
+};
